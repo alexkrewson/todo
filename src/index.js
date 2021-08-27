8dca34a138b405877792c5fb71d2f572145e0336
alexkrewson <alex.krewson@gmail.com>
2021-08-27 11:16:40 -0700

    meaningless change

--- src/index.js
+++ src/index.js
@@ -5,23 +5,7 @@ import { printOnLoad, clearContent, summonTaskInputs, printTop } from './print'
 import { addTask, printAllTasks, printTaskBottom, displayTaskTab, taskList, taskFactory } from './task'
 import { addProject, displayProjectTab, projectList } from './project'
 
-// if (localStorage.getItem('task 1 title')) {
-//     console.log('task 1 title')
-//     var existingTaskArray = localStorage['task 1 title']
-//     console.log(existingTaskArray)
-// }
-
-// if (localStorage.length > 0) {
-//     // console.log(localStorage)
-//     localStorage.keys.forEach(function (item) {
-//         console.log(item)
-//     });
-// }
-
-// taskList = []
-// projectList = []
 for (const key in localStorage) {
-    // console.log(`${key}: ${localStorage.getItem(key)}`);
     if (key.includes('task')) {
         var obj = JSON.parse(localStorage.getItem(key))
         console.log(obj)
